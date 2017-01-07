6328997f7a2173c8268a3dcacf4143983e5b1c56
Marcin Nabiałek <dev@nabialek.org>
2017-01-07 20:09:32 +0100

    Add checkbox filter

---
+++ src/Filters/CheckboxFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mnabialek\LaravelHandyRequest\Filters;
+
+use Mnabialek\LaravelHandyRequest\Filters\Base\GlobalFilter;
+
+class CheckboxFilter extends GlobalFilter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function applyGlobal(array $input)
+    {
+        foreach (array_get($this->options, 'only', []) as $field) {
+            array_set($input, $field,  array_get($input, $field, $this->defaultValue()));
+        }
+    }
+
+    /**
+     * Get default value assigned to field
+     *
+     * @return mixed
+     */
+    protected function defaultValue()
+    {
+        return array_get($this->options, 'value', 0);
+    }
+}
