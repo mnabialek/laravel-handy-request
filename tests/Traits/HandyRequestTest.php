8be2d8a2ae4e34bc429eb326dddbcb9e73413087
Marcin Nabiałek <dev@nabialek.org>
2017-01-08 11:30:05 +0100

    Add tests for except option

--- tests/Traits/HandyRequestTest.php
+++ tests/Traits/HandyRequestTest.php
@@ -446,6 +446,307 @@ class HandyRequestTest extends UnitTestCase
         ], $request->all());
     }
 
+
+    /** @test */
+    public function it_applies_valid_trim_filter_when_except_option_used_for_complex_structure()
+    {
+        $input = [
+            'a' => ' aaa ',
+            'b' => ' bbb ',
+            'c' => 2,
+            'd' => [
+                'a' => ' aaa ',
+                'e' => ' eee ',
+                'f' => ' fff ',
+                'g' => [
+                    'a' => ' aaa ',
+                    'b' => ' bbb ',
+                ],
+            ],
+        ];
+
+        $request = $this->initializeRequest($input, new class() extends HandyRequest {
+            protected $filters = [
+                'trim' => [
+                    'except' => ['a', 'd'],
+                ],
+            ];
+        });
+
+        $this->assertEquals([
+            'a' => ' aaa ',
+            'b' => 'bbb',
+            'c' => 2,
+            'd' => [
+                'a' => 'aaa',
+                'e' => 'eee',
+                'f' => 'fff',
+                'g' => [
+                    'a' => 'aaa',
+                    'b' => 'bbb',
+                ],
+            ],
+        ], $request->all());
+    }
+
+    /** @test */
+    public function it_applies_valid_trim_filter_when_except_option_used_in_dot_notation_for_complex_structure()
+    {
+        $input = [
+            'a' => ' aaa ',
+            'b' => ' bbb ',
+            'c' => 2,
+            'd' => [
+                'a' => ' aaa ',
+                'e' => ' eee ',
+                'f' => ' fff ',
+                'g' => [
+                    'a' => ' aaa ',
+                    'b' => ' bbb ',
+                ],
+            ],
+        ];
+
+        $request = $this->initializeRequest($input, new class() extends HandyRequest {
+            protected $filters = [
+                'trim' => [
+                    'except' => ['a', 'd.a'],
+                ],
+            ];
+        });
+
+        $this->assertEquals([
+            'a' => ' aaa ',
+            'b' => 'bbb',
+            'c' => 2,
+            'd' => [
+                'a' => ' aaa ',
+                'e' => 'eee',
+                'f' => 'fff',
+                'g' => [
+                    'a' => 'aaa',
+                    'b' => 'bbb',
+                ],
+            ],
+        ], $request->all());
+    }
+
+    /** @test */
+    public function it_applies_valid_trim_filter_when_except_option_used_for_child_fields_for_complex_structure()
+    {
+        $input = [
+            'a' => ' aaa ',
+            'b' => ' bbb ',
+            'c' => 2,
+            'd' => [
+                'a' => ' aaa ',
+                'e' => ' eee ',
+                'f' => ' fff ',
+                'g' => [
+                    'a' => ' aaa ',
+                    'b' => ' bbb ',
+                ],
+            ],
+        ];
+
+        $request = $this->initializeRequest($input, new class() extends HandyRequest {
+            protected $filters = [
+                'trim' => [
+                    'except' => ['a', 'd.*'],
+                ],
+            ];
+        });
+
+        $this->assertEquals([
+            'a' => ' aaa ',
+            'b' => 'bbb',
+            'c' => 2,
+            'd' => [
+                'a' => ' aaa ',
+                'e' => ' eee ',
+                'f' => ' fff ',
+                'g' => [
+                    'a' => 'aaa',
+                    'b' => 'bbb',
+                ],
+            ],
+        ], $request->all());
+    }
+
+    /** @test */
+    public function it_applies_valid_trim_filter_when_except_option_used_for_successor_fields_for_complex_structure()
+    {
+        $input = [
+            'a' => ' aaa ',
+            'b' => ' bbb ',
+            'c' => 2,
+            'd' => [
+                'a' => ' aaa ',
+                'e' => ' eee ',
+                'f' => ' fff ',
+                'g' => [
+                    'a' => ' aaa ',
+                    'b' => ' bbb ',
+                ],
+            ],
+        ];
+
+        $request = $this->initializeRequest($input, new class() extends HandyRequest {
+            protected $filters = [
+                'trim' => [
+                    'except' => ['a', 'd.**'],
+                ],
+            ];
+        });
+
+        $this->assertEquals([
+            'a' => ' aaa ',
+            'b' => 'bbb',
+            'c' => 2,
+            'd' => [
+                'a' => ' aaa ',
+                'e' => ' eee ',
+                'f' => ' fff ',
+                'g' => [
+                    'a' => ' aaa ',
+                    'b' => ' bbb ',
+                ],
+            ],
+        ], $request->all());
+    }
+
+    /** @test */
+    public function it_resolves_contraints_in_valid_way_for_except_option()
+    {
+        $input = [
+            'da' => ' ddd ',
+            'dwa' => [
+                'x' => [
+                    'a' => ' aaa ',
+                    'b' => ' bbb ',
+                ],
+            ],
+            'd' => [
+                'a' => ' aaa ',
+                'e' => ' eee ',
+                'f' => ' fff ',
+                'g' => [
+                    'a' => ' aaa ',
+                    'b' => ' bbb ',
+                    'x' => [
+                        'a' => ' aaa ',
+                        'b' => ' bbb ',
+                    ],
+                    'ax' => [
+                        'a' => ' aaa ',
+                        'b' => ' bbb ',
+                    ],
+                ],
+            ],
+        ];
+
+        $request = $this->initializeRequest($input, new class() extends HandyRequest {
+            protected $filters = [
+                'trim' => [
+                    'except' => ['d.*.x.**'],
+                ],
+            ];
+        });
+
+        $this->assertEquals([
+            'da' => 'ddd',
+            'dwa' => [
+                'x' => [
+                    'a' => 'aaa',
+                    'b' => 'bbb',
+                ],
+            ],
+            'd' => [
+                'a' => 'aaa',
+                'e' => 'eee',
+                'f' => 'fff',
+                'g' => [
+                    'a' => 'aaa',
+                    'b' => 'bbb',
+                    'x' => [
+                        'a' => ' aaa ',
+                        'b' => ' bbb ',
+                    ],
+                    'ax' => [
+                        'a' => 'aaa',
+                        'b' => 'bbb',
+                    ],
+                ],
+            ],
+        ], $request->all());
+    }
+
+    /** @test */
+    public function it_applies_valid_trim_filter_when_except_option_used_for_array_for_complex_structure()
+    {
+        $input = [
+            'a' => ' aaa ',
+            'b' => ' bbb ',
+            'c' => 2,
+            'd' => [
+                'a' => ' aaa ',
+                'e' => ' eee ',
+                'f' => ' fff ',
+                'name' => ' this is d name ',
+                'surname' => ' This is d surname ',
+                'g' => [
+                    'a' => ' aaa ',
+                    'b' => ' bbb ',
+                    'name' => ' this is g name ',
+                    'surname' => ' This is g surname ',
+                ],
+                [
+                    'name' => ' this is 1 st name ',
+                    'surname' => ' This is 1st surname ',
+                ],
+                [
+                    'name' => ' this is 2nd name ',
+                    'surname' => ' This is 2nd surname ',
+                ],
+            ],
+        ];
+
+        $request = $this->initializeRequest($input, new class() extends HandyRequest {
+            protected $filters = [
+                'trim' => [
+                    'except' => ['a', 'd.*.name'],
+                ],
+            ];
+        });
+
+        $this->assertEquals([
+            'a' => ' aaa ',
+            'b' => 'bbb',
+            'c' => 2,
+            'd' => [
+                'a' => 'aaa',
+                'e' => 'eee',
+                'f' => 'fff',
+                'name' => 'this is d name',
+                'surname' => 'This is d surname',
+                'g' => [
+                    'a' => 'aaa',
+                    'b' => 'bbb',
+                    'name' => ' this is g name ',
+                    'surname' => 'This is g surname',
+                ],
+                [
+                    'name' => ' this is 1 st name ',
+                    'surname' => 'This is 1st surname',
+                ],
+                [
+                    'name' => ' this is 2nd name ',
+                    'surname' => 'This is 2nd surname',
+                ],
+            ],
+        ], $request->all());
+    }
+
     protected function initializeRequest(array $input, $class)
     {
         $request = new Request($input);
