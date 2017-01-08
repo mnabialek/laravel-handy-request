74d937bc4464351606577d127ab8a28c8323bbee
Marcin Nabiałek <dev@nabialek.org>
2017-01-08 09:13:07 +0100

    Cleanup tests code

--- tests/Filters/NullableFilterTest.php
+++ tests/Filters/NullableFilterTest.php
@@ -7,50 +7,51 @@ use Tests\UnitTestCase;
 
 class NullableFilterTest extends UnitTestCase
 {
+    /**
+     * @var NullableFilter
+     */
+    protected $filter;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->filter = new NullableFilter();
+        $this->filter->setOptions([]);
+    }
+
     /** @test */
     public function it_shows_its_not_a_global_filter()
     {
-        $filter = new NullableFilter();
-        $this->assertFalse($filter->isGlobal());
+        $this->assertFalse($this->filter->isGlobal());
     }
 
     /** @test */
     public function it_doesnt_change_non_empty_value()
     {
-        $filter = new NullableFilter();
-        $filter->setOptions([]);
-        $this->assertSame(3, $filter->apply(3, 'test'));
+        $this->assertSame(3, $this->filter->apply(3, 'test'));
     }
 
     /** @test */
     public function it_does_change_empty_string_into_null()
     {
-        $filter = new NullableFilter();
-        $filter->setOptions([]);
-        $this->assertNull($filter->apply('', 'test'));
+        $this->assertNull($this->filter->apply('', 'test'));
     }
 
     /** @test */
     public function it_does_change_boolean_false_into_null()
     {
-        $filter = new NullableFilter();
-        $filter->setOptions([]);
-        $this->assertNull($filter->apply(false, 'test'));
+        $this->assertNull($this->filter->apply(false, 'test'));
     }
 
     /** @test */
     public function it_does_change_integer_0_into_null()
     {
-        $filter = new NullableFilter();
-        $filter->setOptions([]);
-        $this->assertNull($filter->apply(0, 'test'));
+        $this->assertNull($this->filter->apply(0, 'test'));
     }
 
     /** @test */
     public function it_does_not_change_not_trimmed_empty_string()
     {
-        $filter = new NullableFilter();
-        $filter->setOptions([]);
-        $this->assertSame(' ', $filter->apply(' ', 'test'));
+        $this->assertSame(' ', $this->filter->apply(' ', 'test'));
     }
 }
--- tests/Filters/TrimFilterTest.php
+++ tests/Filters/TrimFilterTest.php
@@ -7,42 +7,45 @@ use Tests\UnitTestCase;
 
 class TrimFilterTest extends UnitTestCase
 {
+    /**
+     * @var TrimFilter
+     */
+    protected $filter;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->filter = new TrimFilter();
+        $this->filter->setOptions([]);
+    }
+
     /** @test */
     public function it_shows_its_not_a_global_filter()
     {
-        $filter = new TrimFilter();
-        $this->assertFalse($filter->isGlobal());
+        $this->assertFalse($this->filter->isGlobal());
     }
 
     /** @test */
     public function it_trims_non_trimmed_string()
     {
-        $filter = new TrimFilter();
-        $filter->setOptions([]);
-        $this->assertSame('3', $filter->apply(' 3 ', 'test'));
+        $this->assertSame('3', $this->filter->apply(' 3 ', 'test'));
     }
 
     /** @test */
     public function it_returns_original_string_when_its_already_trimmed()
     {
-        $filter = new TrimFilter();
-        $filter->setOptions([]);
-        $this->assertSame('3a3', $filter->apply('3a3', 'test'));
+        $this->assertSame('3a3', $this->filter->apply('3a3', 'test'));
     }
 
     /** @test */
     public function it_doesnt_trim_boolean_value()
     {
-        $filter = new TrimFilter();
-        $filter->setOptions([]);
-        $this->assertSame(false, $filter->apply(false, 'test'));
+        $this->assertSame(false, $this->filter->apply(false, 'test'));
     }
 
     /** @test */
     public function it_doesnt_trim_integer_value()
     {
-        $filter = new TrimFilter();
-        $filter->setOptions([]);
-        $this->assertSame(0, $filter->apply(0, 'test'));
+        $this->assertSame(0, $this->filter->apply(0, 'test'));
     }
 }
